cc70049dc34481e1756bd7a3165616d20b10f738
HyeonWooGa <yeonwoopark22@gmail.com>
2022-08-29 23:12:47 +0900

    💻 프론트엔드 설정

--- src/server.js
+++ src/server.js
@@ -2,6 +2,16 @@ import express from "express";
 
 const app = express();
 
-console.log("Hello :)");
+// 뷰 엔진 설정
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
 
-app.listen(3000);
+// 유저에게 파일공유
+app.use("/public", express.static(__dirname + "/public"));
+
+// 라우터
+app.get("/", (req, res) => res.render("home"));
+
+// 앱.리슨
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen);
