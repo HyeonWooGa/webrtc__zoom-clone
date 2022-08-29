2651457558806f900db11ea713eb6a245ad340ef
HyeonWooGa <yeonwoopark22@gmail.com>
2022-08-29 23:31:34 +0900

    💻 CatchAll URL 설정

--- src/server.js
+++ src/server.js
@@ -6,11 +6,12 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 
-// 유저에게 파일공유
+// 퍼블릭 폴더에 유저가 접근하지 못하게 설정
 app.use("/public", express.static(__dirname + "/public"));
 
 // 라우터
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 // 앱.리슨
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
